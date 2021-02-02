f53c48294fafa7390b3c3d21fdbb214abbd9988f
Tommy Crafton <twcrafton@gmail.com>
2021-02-02 08:18:46 -0600

    Setup app for Heroku

--- public/js/app.js
+++ public/js/app.js
@@ -4,18 +4,16 @@ const messageOne = document.querySelector("#message-1");
 const messageTwo = document.querySelector("#message-2");
 
 const getWeather = (location) =>
-  fetch(`http://localhost:3000/weather?address=${location}`).then(
-    (response) => {
-      response.json().then((data) => {
-        if (!data.error) {
-          messageOne.textContent = data.location;
-          messageTwo.textContent = data.forecast;
-        } else {
-          messageOne.textContent = data.error;
-        }
-      });
-    }
-  );
+  fetch(`/weather?address=${location}`).then((response) => {
+    response.json().then((data) => {
+      if (!data.error) {
+        messageOne.textContent = data.location;
+        messageTwo.textContent = data.forecast;
+      } else {
+        messageOne.textContent = data.error;
+      }
+    });
+  });
 
 weatherForm.addEventListener("submit", (e, callback) => {
   e.preventDefault();
