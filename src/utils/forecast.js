c5c2c9e7f31f48d20f41a5a23e8405663ef13631
Tommy Crafton <twcrafton@gmail.com>
2021-02-02 07:08:13 -0600

    Initial commit

---
+++ src/utils/forecast.js
@@ -0,0 +1,22 @@
+const request = require("request");
+
+const forecast = (latitude, longitude, callback) => {
+  const url = `http://api.weatherstack.com/current?access_key=74a21342667eacd9a4618f4c89869ec1&query=${latitude},${longitude}&units=f`;
+
+  request({ url, json: true }, (error, { body }) => {
+    if (error) {
+      callback("Unable to connect to weather service.");
+    } else if (body.error) {
+      callback("Unable to find location, try other coordinates.");
+    } else {
+      let data = body.current;
+
+      callback(
+        undefined,
+        `${data.weather_descriptions[0]}. It is currently ${data.temperature}.  It feels like ${data.feelslike} degrees out.`
+      );
+    }
+  });
+};
+
+module.exports = forecast;
